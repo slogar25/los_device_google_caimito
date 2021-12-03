b5e8b1da1e8c08d4edf3a7c895d16b6ad301c3f1
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-04 01:01:42 +0530

    caimito: Set up proprietary files extraction
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    
    Co-authored-by: Aaron Kling <webgeek1234@gmail.com>
    Co-authored-by: Cosmin Tanislav <demonsingur@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I17b78ccf212d8364070c40fe3c7d06d4048e3fe4

--- device-lineage.mk
+++ device-lineage.mk
@@ -6,7 +6,33 @@
 
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay-lineage
 
+# ANGLE - Almost Native Graphics Layer Engine
+PRODUCT_PACKAGES += \
+    ANGLE
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.mep.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.mep.xml \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# Build necessary packages for vendor
+
+# Codec2
+PRODUCT_PACKAGES += \
+    libexynosv4l2
+
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.location.gps.prebuilt.xml \
+    liblzma.vendor \
+    libunwindstack.vendor
+
+# Graphics
+PRODUCT_PACKAGES += \
+    libEGL_angle \
+    libGLESv1_CM_angle \
+    libGLESv2_angle
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.dynamic_sensor_hal
