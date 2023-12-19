9c71cd3f28492d3f7fb7dafcdaaf758738994b62
Wilson Sung <wilsonsung@google.com>
2023-12-19 03:19:21 +0000

    Move fingerprint to each device sepolicy folder
    Bug: 312322769
    Test: make sepolicy
    Change-Id: Id3f76b1e3c2d7e00a4a84028555de372a79aa8e4

--- ripcurrent24-sepolicy.mk
+++ ripcurrent24-sepolicy.mk
@@ -1,11 +1,5 @@
 # sepolicy that are shared among devices using ZumaPro
 BOARD_SEPOLICY_DIRS += device/google/caimito-sepolicy/ripcurrent24
 
-# UDFPS sepolicy.
-BOARD_SEPOLICY_DIRS += device/google/caimito-sepolicy/fingerprint
-
 # GPS sepolicy
 BOARD_SEPOLICY_DIRS += device/google/caimito-sepolicy/gnss
-
-#Fingerprint
-BOARD_SEPOLICY_DIRS += device/google/caimito-sepolicy/fingerprint_capacitance
--- ripcurrentpro-sepolicy.mk
+++ ripcurrentpro-sepolicy.mk
@@ -1,5 +1,3 @@
 # sepolicy that are shared among devices using ZumaPro
 BOARD_SEPOLICY_DIRS += device/google/caimito-sepolicy/ripcurrentpro
 
-# UDFPS sepolicy.
-BOARD_SEPOLICY_DIRS += device/google/caimito-sepolicy/fingerprint
